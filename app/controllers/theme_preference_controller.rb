f4175512b3a14462446112556ead1b06323b9855
Hex <hecksadecimal@outlook.com>
2021-12-25 05:34:39 -0500

    Fix tests

--- app/controllers/theme_preference_controller.rb
+++ app/controllers/theme_preference_controller.rb
@@ -1,13 +1,15 @@
 class ThemePreferenceController < ApplicationController
   def update
     if Rails.application.config.themes.include? theme_params
-      cookies[:theme] = theme_params
+      cookies.permanent[:theme] = theme_params
+      respond_to do |format|
+        format.html { redirect_back fallback_location: root_path }
+      end
     else
       cookies[:theme] = nil
-    end
-
-    respond_to do |format|
-      format.html { redirect_back fallback_location: root_path }
+      respond_to do |format|
+        format.html { redirect_back fallback_location: root_path, status: :bad_request }
+      end
     end
   end
 
--- test/controllers/theme_preference_controller_test.rb
+++ test/controllers/theme_preference_controller_test.rb
@@ -1,8 +1,16 @@
 require "test_helper"
 
 class ThemePreferenceControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get theme_preference_index_url
-    assert_response :success
+
+  Rails.application.config.themes.each do |theme|
+    test "should get update with " + theme do
+      post theme_path, params: { theme: theme }
+      assert_response :redirect
+    end
+  end
+
+  test "should get update with invalid theme" do
+    post theme_path, params: { theme: "invalid" }
+    assert_response :bad_request
   end
 end
