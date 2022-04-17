e257490009b9df9a20faa27365dd90b59e930785
Hex <hecksadecimal@outlook.com>
2022-04-17 16:46:08 -0400

    Add unleash context to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@
 class ApplicationController < ActionController::Base
   before_action :set_start_time
   before_action :set_sentry_context
+  before_action :set_unleash_context
 
   def set_start_time
     @start_time = Time.now.to_f
@@ -12,5 +13,20 @@ class ApplicationController < ActionController::Base
     return if current_user.nil?
 
     Sentry.set_user(username: current_user.username, email: current_user.email, id: current_user.id)
+  end
+
+  def set_unleash_context
+    @unleash_context = if current_user.nil?
+                         Unleash::Context.new(
+                           session_id: session.id,
+                           remote_address: request.remote_ip
+                         )
+                       else
+                         Unleash::Context.new(
+                           session_id: session.id,
+                           remote_address: request.remote_ip,
+                           user_id: current_user.id
+                         )
+                       end
   end
 end
