878dff0887d42b55249982231b17963388058961
Hex <hecksadecimal@outlook.com>
2022-04-17 23:33:38 -0400

    Appease the cop

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -12,8 +12,25 @@ module ApplicationHelper
     end
   end
 
+  def require_active_user
+    return if !banned? && !deleted?
+
+    respond_to do |format|
+      format.html { redirect_to root_url, notice: 'You are not authorized to perform this action.' }
+      format.json { render json: {}, status: :unauthorized }
+    end
+  end
+
   def admin?
     user_signed_in? && current_user.admin?
+  end
+
+  def banned?
+    user_signed_in? && !current_user.unban_at.nil?
+  end
+
+  def deleted?
+    user_signed_in? && !current_user.delete_at.nil?
   end
 
   # User can update given model
