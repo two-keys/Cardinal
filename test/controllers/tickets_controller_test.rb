f116174483630a4942ac76a4b233f73b326865ca
keys <dandys1000@gmail.com>
2022-07-24 21:10:44 -0500

    Appease cops

--- test/controllers/tickets_controller_test.rb
+++ test/controllers/tickets_controller_test.rb
@@ -1,4 +1,6 @@
-require "test_helper"
+# frozen_string_literal: true
+
+require 'test_helper'
 
 class TicketsControllerTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
