fdcf7cff0a337717a06e725f4fb430b2c97f4577
two-keys <dandys10@live.com>
2024-06-29 17:36:13 -0500

    Disallow adding someone else's character to your prompts

--- app/models/object_character.rb
+++ app/models/object_character.rb
@@ -5,6 +5,7 @@ class ObjectCharacter < ApplicationRecord
   belongs_to :character
 
   validates :character_id, uniqueness: { scope: %i[object_id object_type] }
+  validate :authorization, on: %i[create update]
 
   # This should be called by using the object_characters setter on a prompt
   # since we aren't setting Objectcharacter.prompt here
@@ -20,5 +21,13 @@ class ObjectCharacter < ApplicationRecord
 
     # Let @prompt.save handle the bulk of processing/validating characters
     new_object_characters
+  end
+
+  private
+
+  def authorization
+    return if object.user.id == character.user.id
+
+    errors.add(:user, "You don't own this #{object_type} or Character.")
   end
 end
