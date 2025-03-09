fd992fb3ca770db7b9655dfb4c1e971b3dbdfb16
Hex <hecksadecimal@outlook.com>
2025-03-09 04:27:19 -0400

    Switch to an updated fork of twemoji

--- app/javascript/controllers/twemoji_controller.js
+++ app/javascript/controllers/twemoji_controller.js
@@ -3,8 +3,7 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
     twemoji.parse(
-        this.element,
-        { base: 'https://cdn.jsdelivr.net/gh/twitter/twemoji@latest/assets/' }
+        this.element
     );
   }
 }
