e3210b3b5767d2a1fa0e411fa418d604ad19b62d
Hex <hecksadecimal@outlook.com>
2025-03-13 01:59:36 -0400

    maybe some cors will help

--- Gemfile
+++ Gemfile
@@ -158,3 +158,5 @@ group :test do
   gem 'selenium-webdriver'
   gem 'webdrivers'
 end
+
+gem "rack-cors", "~> 2.0"
---
+++ config/initializers/cors.rb
@@ -0,0 +1,11 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    base_url = ENV['BASE_URL'] || nil
+    if base_url.nil?
+      origins /^https?:\/\/localhost:3000/
+    else
+      origins "https://#{ENV.fetch('BASE_URL')}", "http://#{ENV.fetch('BASE_URL')}"
+    end
+    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+  end
+end
