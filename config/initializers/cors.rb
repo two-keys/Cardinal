3df31570ffff0634508af1293d321eb2f913f70d
Hex <hecksadecimal@outlook.com>
2025-03-13 01:59:52 -0400

    cop appeasement

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,11 +1,13 @@
+# frozen_string_literal: true
+
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     base_url = ENV['BASE_URL'] || nil
     if base_url.nil?
-      origins /^https?:\/\/localhost:3000/
+      origins(%r{^https?://localhost:3000})
     else
       origins "https://#{ENV.fetch('BASE_URL')}", "http://#{ENV.fetch('BASE_URL')}"
     end
-    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+    resource '*', headers: :any, methods: %i[get post patch put]
   end
 end
