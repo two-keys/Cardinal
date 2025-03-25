d69530964672efbfa3c5737e2515b95ae39e0377
Hex <hecksadecimal@outlook.com>
2025-03-25 00:16:51 -0400

    silence cop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class User < ApplicationRecord
+class User < ApplicationRecord # rubocop:disable Metrics/ClassLength
   include Auditable
 
   # Include default devise modules. Others available are:
