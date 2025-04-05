4433328951e3bec09327e0f96ea15c68e666598c
two-keys <dandys10@live.com>
2025-04-05 15:28:28 -0500

    Appease cops

--- db/migrate/20250405073350_add_composite_key_to_prompts.rb
+++ db/migrate/20250405073350_add_composite_key_to_prompts.rb
@@ -1,6 +1,5 @@
 class AddCompositeKeyToPrompts < ActiveRecord::Migration[7.2]
   def change
-    add_index :filters, [:target_type, :target_id, :filter_type, :group, :user_id, :priority]
     add_index :prompts, [:bumped_at]
     add_index :prompts, [:bumped_at, :status, :user_id, :managed], unique: false
   end
