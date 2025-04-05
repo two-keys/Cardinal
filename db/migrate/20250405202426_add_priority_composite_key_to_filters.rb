34162eb3465095ccd5fa287fce48368c09016fe6
two-keys <dandys10@live.com>
2025-04-05 15:28:28 -0500

    Appease cops

--- db/migrate/20250405202426_add_priority_composite_key_to_filters.rb
+++ db/migrate/20250405202426_add_priority_composite_key_to_filters.rb
@@ -1,4 +1,5 @@
 class AddPriorityCompositeKeyToFilters < ActiveRecord::Migration[7.2]
   def change
+    add_index :filters, [:target_type, :target_id, :filter_type, :group, :user_id, :priority]
   end
 end
