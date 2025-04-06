da9ff9eaa75a91489671e4efa401d8550e50c837
two-keys <dandys10@live.com>
2025-04-06 00:22:42 -0500

    Fix advanced search

--- app/controllers/concerns/taggable_controller.rb
+++ app/controllers/concerns/taggable_controller.rb
@@ -3,20 +3,15 @@
 module TaggableController
   extend ActiveSupport::Concern
 
-  # there's probably a better way to do this but my brain is fried
-  included do
-    before_action :set_tag_schema, only: %i[create update_tags]
+  def tag_schema
+    @tag_schema || (@tag_schema = TagSchema.get_schema(controller_name))
   end
 
   private
 
-  def set_tag_schema
-    @tag_schema = TagSchema.get_schema(controller_name)
-  end
-
   def tag_params
-    # logger.debug "#{controller_name} my schema name is '#{@tag_schema.name}'"
+    # logger.debug "#{controller_name} my schema name is '#{tag_schema.name}'"
 
-    params.require(:tags).permit(**@tag_schema.allowed_type_params)
+    params.require(:tags).permit(**tag_schema.allowed_type_params)
   end
 end
