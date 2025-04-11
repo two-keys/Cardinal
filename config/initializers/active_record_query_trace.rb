67fa7f47e39e95e2f254044308288e71c66dc71a
Hex <hecksadecimal@outlook.com>
2025-04-11 18:25:05 -0400

    environment variables for active record query tracing

--- config/initializers/active_record_query_trace.rb
+++ config/initializers/active_record_query_trace.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 if Rails.env.development?
-  ActiveRecordQueryTrace.enabled = true
+  ActiveRecordQueryTrace.enabled = ENV.fetch('QUERY_TRACE', 0).to_i == 1
   ActiveRecordQueryTrace.colorize = :yellow
-  ActiveRecordQueryTrace.lines = 10
+  ActiveRecordQueryTrace.lines = ENV.fetch('QUERY_TRACE_LINES', 5).to_i
 end
