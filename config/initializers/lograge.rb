046b3e88caf7bebac849dbb8e4d7a07469413c7c
Hex <hecksadecimal@outlook.com>
2025-04-14 01:02:43 -0400

    forgot that prod does not need lograge

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.configure do
+  return if Rails.env.production?
+
   config.lograge.custom_options = lambda do |event|
     exceptions = %w[controller action format id]
     {
